fa2448006531564945744a085d5aab6660ecf952
APSK22 <ajsingh2722@gmail.com>
2025-04-24 09:11:15 +0530

    changed somethings about sentry

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -8,7 +8,12 @@ export default function GlobalError({ error }: { error: Error & { digest?: strin
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
-
+  
+  Sentry.replayIntegration({
+    // NOTE: This will disable built-in masking. Only use this if your site has no sensitive data, or if you've already set up other options for masking or blocking relevant data, such as 'ignore', 'block', 'mask' and 'maskFn'.
+    maskAllText: false,
+    blockAllMedia: false,
+  });
   return (
     <html>
       <body>
